0a290c8a180828efd72bf6c7414f80ac89e37aae
kevas007 <kevassiobo@gmail.com>
2021-11-30 15:03:53 +0100

    base

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="flex flex-wrap w-full font relative">
+     <div class="fonts">
+        <div class=" w-full center imge" v-for="(item , index) in imgs" :key="item">
+        <transition name="fade">
+          <img  v-if="currentSlide == index" v-bind:src="item" alt />
+        </transition>
+      </div>
+      <div class="w-full" style="height:20px">
+        <div class="absolute ico bottom-0 flex w-full justify-center">
+          <div
+            v-for="(item, index ) in imgs"
+            :key="item"
+            @click="makeActive(index)"
+            class="w-10 h-10 mx-2 rounded-full cursor-pointer shadow-md"
+          >
+            <div
+              class="w-4 h-4 mx-2 rounded-full cursor-pointer shadow-md flex justify-center items-center"
+              v-for="(icon , i) in icone"
+              :key="icon"
+            >
+              <img
+                class="icones"
+                :class="currentSlide == index ?  'bg-gray-300':  'bg-white' "
+                v-if="index == i"
+                :src="icon"
+                alt
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+     </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  methods: {
+    makeActive(index) {
+      this.currentSlide = index;
+    }
+  }, data() {
+    return {
+      imgs: [
+        require('./assets/images/image-john.jpg'),
+        require('./assets/images/image-tanya.jpg')
+      ],
+      icone: [
+        require('./assets/images/icon-prev.svg'),
+        require('./assets/images/icon-next.svg'),
+
+      ],
+      currentSlide: 0,
+      interval: "",
+      isTitle: true,
+    }
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.currentSlide = this.currentSlide == 1 ? 0 : this.currentSlide + 1;
+    }, 15000);
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+
 }
 </script>
 
@@ -24,5 +81,54 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1s ease;
+}
+.font{
+    background-image: url('./assets/images/pattern-bg.svg');
+
+  width: 700px;
+  height: 100px;
+}
+.fonts{
+    background-image: url('./assets/images/pattern-quotes.svg');
+    width: 700px;
+  height: 100px;
+
+}
+
+.fade-enter-from {
+  opacity: 0;
+  transform: translate(-100%);
+}
+.fade-leave-to {
+  opacity: 0;
+  transform: translate(100%);
+}
+.imge{
+  position: absolute;
+  overflow: hidden;
+  /* right:10; */
+}
+.imge img{
+height:100%;
+width:350px;
+margin-left: 50%;
+margin-top: 8%;
+}
+.ico{
+  top: 450px;
+  left: 70px;
+  z-index: 1;
+  background: #2c3e50;
+}
+.icones {
+  width: 100%;
+  height: 30px;
+  top:51%;
+
 }
 </style>
